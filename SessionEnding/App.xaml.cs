776db60a7dc42db715494e2b2187867d05adbf8f
Burl <burl@burlrice.com>
2022-02-04 07:33:28 -0600

    Add project files.

---
+++ SessionEnding/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace SessionEnding
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_SessionEnding(object sender, SessionEndingCancelEventArgs e)
+        {
+            string msg = string.Format("{0}. End session?", e.ReasonSessionEnding);
+            MessageBoxResult result = MessageBox.Show(msg, "Session Ending", MessageBoxButton.YesNo);
+
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
